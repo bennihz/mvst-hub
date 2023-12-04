a7c4140a66f3c490fcd2ccc1ab953418daf08177
Benjamin Hadizamani <benjamin.hadizamani@tum.de>
2023-12-04 08:49:53 +0100

    feat: proxy url

--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001/graphql-proxy';
+const BASE_URL = 'https://mvst-hub-proxy.onrender.com/graphql-proxy';
 
 export const getUserRepos = async (username: string): Promise<any[]> => {
     const query = `
