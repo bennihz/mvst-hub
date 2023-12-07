3e294d18abe5d8dbad60f4e4bf3e4046e00acbc9
Benjamin Hadizamani <benjamin.hadizamani@tum.de>
2023-12-07 02:13:40 +0100

    feat: client side pagination using asynchronous repo fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,126 +1,141 @@
+// app.tsx
 import React, { useState, useEffect } from 'react'
 import RepositoryList from './components/RepositoryList'
 import Error from './components/Error'
-import { getUserRepos, getUserInfo } from './services/githubService'
+import {
+    getUserReposLimited,
+    getUserInfo,
+    getUserReposAll,
+} from './services/githubService'
 import Navbar from './components/Navbar'
 import SearchBarBig from './components/SearchBarBig'
 import UserOverview from './components/UserOverview'
 import Pagination from './components/Pagination'
 import Search from './components/Search'
-import NoRepositoriesFound from './stories/NoRepositoriesFound'
 
 const App: React.FC = () => {
-    const [loading, setLoading] = useState<boolean>(false)
+    const [reposLoading, setReposLoading] = useState<boolean>(false)
     const [error, setError] = useState<string | null>(null)
     const [username, setUsername] = useState<string>('')
-    const [repositories, setRepositories] = useState<any[]>([])
     const [userInfo, setUserInfo] = useState<any | null>(null)
-    const [hasNextPage, setHasNextPage] = useState<any | null>(true)
-    const [hasPreviousPage, setHasPreviousPage] = useState<any | null>(false)
-    const [endCursorArray, setEndCursorArray] = useState<string[]>([])
+    const [hasNextPage, setHasNextPage] = useState<boolean>(false)
+    const [hasPreviousPage, setHasPreviousPage] = useState<boolean>(false)
+
     const [repoNameFilter, setRepoNameFilter] = useState<string>('')
     const [repoLanguageFilter, setRepoLanguageFilter] = useState<string>('')
 
-    useEffect(() => {}, [repositories])
+    const [filteredRepositories, setFilteredRepositories] = useState<any[]>([])
+    const [allRepositories, setAllRepositories] = useState<any[]>([])
+    const [displayedRepositories, setDisplayedRepositories] = useState<any[]>(
+        [],
+    )
+    const [page, setPage] = useState<number>(0)
+
+    const fetchAllRepositories = async () => {
+        try {
+            const allRepoData = await getUserReposAll(username)
+            setAllRepositories(allRepoData.repositories)
+            setFilteredRepositories(allRepoData.repositories)
+
+            setReposLoading(false)
+            setError(null)
+        } catch (error) {
+            console.error('Error fetching all repositories', error)
+            setReposLoading(false)
+            setError('Error fetching all repositories.')
+        }
+    }
+
+    const fetchInitialRepositories = async () => {
+        try {
+            setReposLoading(true)
+            await new Promise((resolve) => setTimeout(resolve, 200))
+
+            const initialRepoData = await getUserReposLimited(username, 10)
+            setDisplayedRepositories(initialRepoData.repositories)
+            setHasNextPage(initialRepoData.hasNextPage)
+
+            setReposLoading(false)
+
+            await fetchAllRepositories()
+
+            setError(null)
+        } catch (error) {
+            console.error('Error fetching data', error)
+            setReposLoading(false)
+            setError('Error fetching data.')
+        }
+    }
 
     useEffect(() => {
-        setEndCursorArray([])
+        setUserInfo(null)
         if (username.trim() !== '') {
             fetchUser()
-            fetchRepositories()
+            fetchInitialRepositories()
         }
         setRepoNameFilter('')
         setRepoLanguageFilter('')
     }, [username])
 
-    useEffect(() => {
-        setEndCursorArray([])
-        console.log('changed filter', repoNameFilter)
-        fetchRepositories()
-    }, [repoNameFilter])
-
-    const handleUserSearch = async (username: string) => {
-        setUsername(username)
-        setEndCursorArray([])
-    }
-
     const fetchUser = async () => {
         try {
-            setLoading(true)
-
             const userData = await getUserInfo(username)
-
             setUserInfo(userData)
-
-            setLoading(false)
             setError(null)
         } catch (error) {
             console.error('Error fetching data', error)
-            setLoading(false)
             setError('Error fetching data.')
         }
     }
 
-    const fetchRepositories = async () => {
-        try {
-            setLoading(true)
+    const handlePageChange = (pageChange: number) => {
+        const newPage = page + pageChange
 
-            let repoData = null
+        // update hasNextPage and hasPreviousPage
+        setHasPreviousPage(newPage > 0)
+        setHasNextPage(allRepositories.length > (newPage + 1) * 10)
 
-            if (endCursorArray.length === 0) {
-                repoData = await getUserRepos(username, repoNameFilter, null)
-            } else {
-                repoData = await getUserRepos(
-                    username,
-                    repoNameFilter,
-                    endCursorArray[endCursorArray.length - 1]
-                )
-            }
-
-            console.log(repoData)
-            setRepositories(repoData.repositories)
-            setEndCursorArray([...endCursorArray, repoData.endCursor])
-            setHasNextPage(repoData.hasNextPage)
-            setHasPreviousPage(repoData.hasPreviousPage)
-            setLoading(false)
-            setError(null)
-        } catch (error) {
-            console.error('Error fetching data', error)
-            setLoading(false)
-            setError('Error fetching data.')
-        }
+        // update filteredRepositories based on the current page
+        const startIndex = newPage * 10
+        const endIndex = startIndex + 10
+        console.log(filteredRepositories.slice(startIndex, endIndex))
+        setDisplayedRepositories(
+            filteredRepositories.slice(startIndex, endIndex),
+        )
+        setPage(newPage)
     }
 
-    const handlePageChange = async (page: number) => {
-        if (page > 0) {
-            await fetchRepositories()
-        } else {
-            endCursorArray.pop()
-            endCursorArray.pop()
-            await fetchRepositories()
-        }
-    }
-
-    const handleRepoNameFilter = async (text: string) => {
-        console.log(text)
+    const handleRepoNameFilter = (text: string) => {
         if (text.trim() === '') {
             return
         }
         setRepoNameFilter(text)
     }
 
-    const handleRepoLangFilter = async (text: string) => {
+    const handleRepoLangFilter = (text: string) => {
         if (text.trim() === '') {
             return
         }
         setRepoLanguageFilter(text)
     }
 
+    const handleUserSearch = (text: string) => {
+        if (text.trim() === '') {
+            return
+        }
+        setUsername(text)
+        setPage(0)
+        setHasNextPage(false)
+        setHasNextPage(false)
+        setFilteredRepositories([])
+        setAllRepositories([])
+        setDisplayedRepositories([])
+    }
+
     return (
         <>
             <Navbar />
             <div className="container mx-auto p-4">
-                {/* User Overview - 2/5 width */}
                 <SearchBarBig onSearch={handleUserSearch} />
                 <div className="container flex">
                     <div className="w-1/3">
@@ -134,7 +149,6 @@ const App: React.FC = () => {
                             />
                         )}
                     </div>
-
                     <div className="w-2/3 pl-4">
                         {userInfo && (
                             <>
@@ -148,18 +162,15 @@ const App: React.FC = () => {
                                         placeholder="Filter language"
                                     />
                                 </div>
-                                {repositories && repositories.length > 0 ? (
-                                    <RepositoryList
-                                        repositories={repositories}
-                                        isLoading={loading}
-                                    />
-                                ) : (
-                                    <NoRepositoriesFound />
-                                )}
+                                <RepositoryList
+                                    repositories={displayedRepositories}
+                                    isLoading={reposLoading}
+                                    page={page}
+                                />
                             </>
                         )}
                         {error && <Error message={error} />}
-                        {repositories.length > 0 && (
+                        {filteredRepositories.length > 0 && (
                             <Pagination
                                 isFirstPage={!hasPreviousPage}
                                 isLastPage={!hasNextPage}
--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -1,22 +1,28 @@
-const BASE_URL = 'https://mvst-hub-proxy.onrender.com';
+const BASE_URL = 'http://localhost:3001'
+
+// 'https://mvst-hub-proxy.onrender.com';
 
 export interface getUserReposResponse {
-  repositories: any[];
-  hasNextPage: boolean;
-  hasPreviousPage: boolean;
-  endCursor: string;
+    repositories: any[]
 }
 
-export const getUserRepos = async (
+export interface getUserReposLimitedResponse {
+    repositories: any[]
+    hasNextPage: boolean
+}
+
+export const getUserReposLimited = async (
     username: string,
-    name: string | null = null,
-    afterCursor: string | null = null,
-    perPage: number = 10
-): Promise<getUserReposResponse> => {
-  const query = `
+    perPage: number,
+): Promise<getUserReposLimitedResponse> => {
+    if (perPage <= 0) {
+        throw new Error('Invalid value for perPage. It must be greater than 0.')
+    }
+
+    const query = `
     query {
       user(login: "${username}") {
-        repositories(first: ${perPage}, after: ${afterCursor !== null ? `"${afterCursor}"` : null}, orderBy: {field: UPDATED_AT, direction: DESC}${name ? `, query: "${name} in:name"` : ''}) {
+        repositories(first: ${perPage}, orderBy: {field: UPDATED_AT, direction: DESC}) {
           nodes {
             id
             name
@@ -26,56 +32,115 @@ export const getUserRepos = async (
             }
           }
           pageInfo {
-            hasNextPage
-            hasPreviousPage
-            endCursor
-            startCursor
-          }
+              hasNextPage
+              hasPreviousPage
+              endCursor
+              startCursor
+            }
         }
       }
     }
-  `;
+  `
 
-  const variables: any = {
-    username,
-    perPage,
-    afterCursor,
-    name,
-  };
+    const variables: any = {
+        username,
+        perPage,
+    }
 
-  const response = await fetch(BASE_URL + '/graphql-proxy', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ query, variables }),
-  });
+    const response = await fetch(BASE_URL + '/graphql-proxy', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query, variables }),
+    })
 
-  if (!response.ok) {
-    throw new Error('Failed to fetch user repositories');
-  }
+    if (!response.ok) {
+        throw new Error('Failed to fetch user repositories')
+    }
 
-  const data = await response.json();
+    const data = await response.json()
 
-  const nodes = data?.data?.user?.repositories?.nodes || [];
-  const pageInfo = data?.data?.user?.repositories?.pageInfo;
-  const hasNextPage = pageInfo?.hasNextPage || false;
-  const endCursor = pageInfo?.endCursor || null;
-  const hasPreviousPage = pageInfo?.hasPreviousPage || false;
+    const repositories = data?.data?.user?.repositories?.nodes || []
+    const hasNextPage =
+        data?.data?.user?.repositories?.pageInfo?.hasNextPage || false
 
-  return { hasPreviousPage, repositories: nodes, endCursor, hasNextPage };
-};
+    return { repositories, hasNextPage }
+}
+
+export const getUserReposAll = async (
+    username: string,
+): Promise<getUserReposResponse> => {
+    let hasNextPage = true
+    let endCursor = null
+    let allRepositories: any[] = []
+
+    while (hasNextPage) {
+        const query = `
+      query {
+        user(login: "${username}") {
+          repositories(first: 100, after: ${
+              endCursor !== null ? `"${endCursor}"` : null
+          }, orderBy: {field: UPDATED_AT, direction: DESC}) {
+            nodes {
+              id
+              name
+              description
+              primaryLanguage {
+                name
+              }
+            }
+            pageInfo {
+              hasNextPage
+              hasPreviousPage
+              endCursor
+              startCursor
+            }
+          }
+        }
+      }
+    `
+
+        const variables: any = {
+            username,
+            perPage: 100,
+            endCursor,
+        }
+
+        const response: any = await fetch(BASE_URL + '/graphql-proxy', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ query, variables }),
+        })
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch user repositories')
+        }
+
+        const data = await response.json()
+        const repositories = data?.data?.user?.repositories?.nodes || []
+
+        allRepositories = [...allRepositories, ...repositories]
+        const pageInfo = data?.data?.user?.repositories?.pageInfo
+        hasNextPage = pageInfo?.hasNextPage || false
+        endCursor = pageInfo?.endCursor || null
+    }
+
+    return { repositories: allRepositories }
+}
 
 interface GitHubUser {
-  login: string;
-  avatarUrl: string;
-  bio: string;
-  location?: string;
-  htmlUrl: string;
+    login: string
+    avatarUrl: string
+    bio: string
+    location?: string
+    htmlUrl: string
 }
 
 export const getUserInfo = async (username: string): Promise<GitHubUser> => {
-  const query = `
+    const query = `
     query {
       user(login: "${username}") {
         login
@@ -85,28 +150,28 @@ export const getUserInfo = async (username: string): Promise<GitHubUser> => {
         url  # Use 'url' instead of 'htmlUrl'
       }
     }
-  `;
+  `
 
-  const response = await fetch(BASE_URL + "/graphql-proxy", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ query }),
-  });
+    const response = await fetch(BASE_URL + '/graphql-proxy', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query }),
+    })
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch user information");
-  }
+    if (!response.ok) {
+        throw new Error('Failed to fetch user information')
+    }
 
-  const { data } = await response.json();
-  const userData = data.user;
+    const { data } = await response.json()
+    const userData = data.user
 
-  return {
-    login: userData.login,
-    avatarUrl: userData.avatarUrl,
-    bio: userData.bio,
-    location: userData.location,
-    htmlUrl: userData.url,
-  };
-};
+    return {
+        login: userData.login,
+        avatarUrl: userData.avatarUrl,
+        bio: userData.bio,
+        location: userData.location,
+        htmlUrl: userData.url,
+    }
+}
