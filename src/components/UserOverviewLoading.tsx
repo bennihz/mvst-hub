c54061dcddcd60c5f654ffc3699291fa554a0e3d
Benjamin Hadizamani <benjamin.hadizamani@tum.de>
2023-12-08 14:31:22 +0100

    fix: skeleton user loading

--- src/components/UserOverviewLoading.tsx
+++ src/components/UserOverviewLoading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const UserOverviewLoading: React.FC = () => {
     return (
         <div className="flex flex-row md:flex-col items-start p-4 border border-neutral-300 rounded-md bg-w dark:bg-zinc-900 dark:border-neutral-800">
-            <div className="mb-2 md:mb-4 w-1/3 md:w-full">
+            <div className="mb-2 mr-4 md:mb-4 w-1/3 md:w-full">
                 <div className="w-32 h-32 animate-pulse rounded-full md:mx-auto bg-gray-200 dark:bg-gray-800 mb-4" />
             </div>
             <div className="w-2/3 md:w-full">
